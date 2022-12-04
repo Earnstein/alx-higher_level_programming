aeb1444424cb9dfc2d302c05d745419b60644794
Bakare <bdamilola00@gmail.com>
2022-12-04 10:44:44 -0800

    completed all task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ table File  9 lines (9 sloc)  238 Bytes
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
